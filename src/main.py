496fee4f00e2eba923dc492775e29f1120bb2b95
Niko Steinhoff <niko.steinhoff@gmail.com>
2015-02-11 14:28:20 +0100

    blank files

---
+++ src/main.py
@@ -0,0 +1,10 @@
+__author__ = "nikosteinhoff"
+__email__ = "niko.steinhoff@gmail.com"
+
+
+def main():
+    print("This is main()")
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/read_data.py
@@ -0,0 +1,6 @@
+__author__ = 'nikosteinhoff'
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
