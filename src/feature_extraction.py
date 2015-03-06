37abe357477294661c5310104856a8a86fbdc949
Niko Steinhoff <niko.steinhoff@gmail.com>
2015-03-06 14:46:19 +0100

    some numpy testing

---
+++ src/feature_extraction.py
@@ -0,0 +1,9 @@
+__author__ = 'nikosteinhoff'
+
+import numpy as np
+
+
+
+
+if __name__ == "__main__":
+    print("Running as main")
--- src/read_data.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'nikosteinhoff'
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
