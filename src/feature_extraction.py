298818da50e1c9525c7eb84391557cc4ffb49018
Niko Steinhoff <niko.steinhoff@gmail.com>
2015-03-15 19:49:54 +0100

    - complete overhaul of the feature extraction - allows for more flexibility in selecting features according to specific conditions

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -10,7 +10,7 @@ import time
 
 
 def build_data_set(driver, rebuild_dataset):
-    path = os.path.join(file_handling._data_directory,'drivers', str(driver), 'data_set.csv')
+    path = os.path.join(file_handling._data_directory, 'drivers', str(driver), 'data_set.csv')
 
     if rebuild_dataset or not os.path.isfile(path):
         driver_data = build_driver_data(driver)
@@ -65,43 +65,210 @@ def build_reference_data(n_drivers=200, n_trips=1, exclude=None):
     return reference_data
 
 
-def extract_trip_features(driver, trip):
-    trip_data = file_handling.load_trip_data(driver, trip)
-    transformed_data = transform_data(trip_data)
+def calculate_features(transformed_data, trip):
+    ### NEW (and improved) features
+    ### Indices:
+    # speed = 0
+    # speed changes = 1
+    # turns = 2
+    # speed*turns = 3
+    # speed * acceleration = 4
+    # acceleration * turns = 5
+    # speed * acceleration * turns = 6
 
     # Duration
     duration = transformed_data.shape[0]
 
     # Length
-    length = transformed_data[:, 4].sum()
+    length = transformed_data[:, 0].sum()
+
+    is_moving = transformed_data[:, 0] > 0
+    is_accelerating = transformed_data[:, 1] > 0
+    is_decelerating = transformed_data[:, 1] < 0
+    is_turning = transformed_data[:, 2] != 0
+    is_moving_and_turning = is_moving * is_turning
+    is_moving_and_accelerating = is_moving * is_accelerating
+    is_accelerating_and_turning = is_accelerating * is_turning
+    is_moving_accelerating_and_turning = is_moving * is_accelerating * is_turning
+
+    while_moving = transformed_data[is_moving]
+    while_accelerating = transformed_data[is_accelerating]
+    while_decelerating = transformed_data[is_decelerating]
+    while_turning = transformed_data[is_turning]
+    while_moving_and_turning = transformed_data[is_moving_and_turning]
+    while_moving_and_accelerating = transformed_data[is_moving_and_accelerating]
+    while_accelerating_and_turning = transformed_data[is_accelerating_and_turning]
+    while_moving_accelerating_and_turning = transformed_data[is_moving_accelerating_and_turning]
+
+    # List to use if trip does not have any valid readings in order to generate statistics
+    fallback_list = [0 for i in range(5)]
+
+    speed_stats = []
+    if is_moving.sum() > 0:
+        speed = while_moving[:, 0]
+        speed_stats.append(speed.mean())
+        speed_stats.append(speed.std())
+
+        speed_percentiles = np.percentile(speed, [10, 25, 50, 75, 90])
+        speed_stats.append(speed_percentiles[2])
+        speed_stats.append(speed_percentiles[3] - speed_percentiles[1])
+        speed_stats.append(speed_percentiles[4] - speed_percentiles[0])
+    else:
+        speed_stats = fallback_list
+
+    acceleration_stats = []
+    if is_accelerating.sum() > 0:
+        acceleration = while_accelerating[:, 1]
+        acceleration_stats.append(acceleration.mean())
+        acceleration_stats.append(acceleration.std())
+
+        acceleration_percentiles = np.percentile(acceleration, [10, 25, 50, 75, 90])
+        acceleration_stats.append(acceleration_percentiles[2])
+        acceleration_stats.append(acceleration_percentiles[3] - acceleration_percentiles[1])
+        acceleration_stats.append(acceleration_percentiles[4] - acceleration_percentiles[0])
+    else:
+        acceleration_stats = fallback_list
+
+    deceleration_stats = []
+    if is_decelerating.sum() > 0:
+        deceleration = while_decelerating[:, 1]
+        deceleration_stats.append(deceleration.mean())
+        deceleration_stats.append(deceleration.std())
+
+        deceleration_percentiles = np.percentile(deceleration, [10, 25, 50, 75, 90])
+        deceleration_stats.append(deceleration_percentiles[2])
+        deceleration_stats.append(deceleration_percentiles[3] - deceleration_percentiles[1])
+        deceleration_stats.append(deceleration_percentiles[4] - deceleration_percentiles[0])
+    else:
+        deceleration_stats = fallback_list
+
+    turning_stats = []
+    if is_turning.sum() > 0:
+        turning = while_turning[:, 2]
+        turning_stats.append(turning.mean())
+        turning_stats.append(turning.std())
+
+        turning_percentiles = np.percentile(turning, [10, 25, 50, 75, 90])
+        turning_stats.append(turning_percentiles[2])
+        turning_stats.append(turning_percentiles[3] - turning_percentiles[1])
+        turning_stats.append(turning_percentiles[4] - turning_percentiles[0])
+    else:
+        turning_stats = fallback_list
+
+    speed_turning_stats = []
+    if is_moving_and_turning.sum() > 0:
+        speed_turning = while_moving_and_turning[:, 3]
+        speed_turning_stats.append(speed_turning.mean())
+        speed_turning_stats.append(speed_turning.std())
+
+        speed_turning_percentiles = np.percentile(speed_turning, [10, 25, 50, 75, 90])
+        speed_turning_stats.append(speed_turning_percentiles[2])
+        speed_turning_stats.append(speed_turning_percentiles[3] - speed_turning_percentiles[1])
+        speed_turning_stats.append(speed_turning_percentiles[4] - speed_turning_percentiles[0])
+    else:
+        speed_turning_stats = fallback_list
+
+    speed_accelerating_stats = []
+    if is_moving_and_accelerating.sum() > 0:
+        speed_accelerating = while_moving_and_accelerating[:, 4]
+        speed_accelerating_stats.append(speed_accelerating.mean())
+        speed_accelerating_stats.append(speed_accelerating.std())
+
+        speed_accelerating_percentiles = np.percentile(speed_accelerating, [10, 25, 50, 75, 90])
+        speed_accelerating_stats.append(speed_accelerating_percentiles[2])
+        speed_accelerating_stats.append(speed_accelerating_percentiles[3] - speed_accelerating_percentiles[1])
+        speed_accelerating_stats.append(speed_accelerating_percentiles[4] - speed_accelerating_percentiles[0])
+    else:
+        speed_accelerating_stats = fallback_list
+
+    accelerating_turning_stats = []
+    if is_accelerating_and_turning.sum() > 0:
+        accelerating_turning = while_accelerating_and_turning[:, 5]
+        accelerating_turning_stats.append(accelerating_turning.mean())
+        accelerating_turning_stats.append(accelerating_turning.std())
+
+        accelerating_turning_percentiles = np.percentile(accelerating_turning, [10, 25, 50, 75, 90])
+        accelerating_turning_stats.append(accelerating_turning_percentiles[2])
+        accelerating_turning_stats.append(accelerating_turning_percentiles[3] - accelerating_turning_percentiles[1])
+        accelerating_turning_stats.append(accelerating_turning_percentiles[4] - accelerating_turning_percentiles[0])
+    else:
+        accelerating_turning_stats = fallback_list
+        
+    moving_accelerating_turning_stats = []
+    if is_moving_accelerating_and_turning.sum() > 0:
+        moving_accelerating_turning = while_moving_accelerating_and_turning[:, 6]
+        moving_accelerating_turning_stats.append(moving_accelerating_turning.mean())
+        moving_accelerating_turning_stats.append(moving_accelerating_turning.std())
+
+        moving_accelerating_turning_percentiles = np.percentile(moving_accelerating_turning, [10, 25, 50, 75, 90])
+        moving_accelerating_turning_stats.append(moving_accelerating_turning_percentiles[2])
+        moving_accelerating_turning_stats.append(
+            moving_accelerating_turning_percentiles[3] - moving_accelerating_turning_percentiles[1])
+        moving_accelerating_turning_stats.append(
+            moving_accelerating_turning_percentiles[4] - moving_accelerating_turning_percentiles[0])
+    else:
+        moving_accelerating_turning_stats = fallback_list
+
+    new_features = np.hstack((trip,
+                              duration,
+                              length,
+                              speed_stats,
+                              acceleration_stats,
+                              deceleration_stats,
+                              turning_stats,
+                              speed_turning_stats,
+                              speed_accelerating_stats,
+                              accelerating_turning_stats,
+                              moving_accelerating_turning_stats))
+
+    return new_features
+
+
+def calculate_features_old(transformed_data, trip):
+
+    # Duration
+    duration = transformed_data.shape[0]
+
+    # Length
+    length = transformed_data[:, 0].sum()
 
     # Means
-    col_means = transformed_data[:, 4:].mean(axis=0)
+    col_means = transformed_data.mean(axis=0)
 
     # Standard deviations
-    col_std = transformed_data[:, 4:].std(axis=0)
+    col_std = transformed_data.std(axis=0)
 
-    # Interquartile ranges
-    col_percentiles = np.percentile(transformed_data[:, 4:], [10, 25, 50, 75, 90], axis=0)
+    # Inter-quartile ranges
+    col_percentiles = np.percentile(transformed_data, [10, 25, 50, 75, 90], axis=0)
     col_iqr = col_percentiles[3] - col_percentiles[1]
     col_10_90_range = col_percentiles[0] - col_percentiles[4]
     col_med = col_percentiles[2]
 
     # A single row of features per trip
     features = np.hstack((trip, duration, length, col_means, col_std, col_iqr, col_10_90_range, col_med))
+
     descriptions = [
-                    'duration', 'length',
-                    'mean_velocity', 'mean_acceleration', 'mean_directional_change',
-                    'mean_velocity*dir_change', 'mean_velocity*acceleration', 'mean_acceleration*dir_change',
-                    'std_velocity', 'std_acceleration', 'std_directional_change',
-                    'std_velocity*dir_change', 'std_velocity*acceleration', 'std_acceleration*dir_change', 
-                    'iqr_velocity', 'iqr_acceleration', 'iqr_directional_change',
-                    'iqr_velocity*dir_change',  'iqr_velocity*acceleration', 'iqr_acceleration*dir_change',
-                    '90pcr_velocity', '90pcr_acceleration', '90pcr_directional_change',
-                    '90pcr_velocity*dir_change',  '90pcr_velocity*acceleration', '90pcr_acceleration*dir_change',
-                    'median_velocity', 'median_acceleration', 'median_directional_change',
-                    'median_velocity*dir_change', 'median_velocity*acceleration', 'median_acceleration*dir_change'
-                    ]
+        'duration', 'length',
+        'mean_velocity', 'mean_acceleration', 'mean_directional_change',
+        'mean_velocity*dir_change', 'mean_velocity*acceleration', 'mean_acceleration*dir_change',
+        'std_velocity', 'std_acceleration', 'std_directional_change',
+        'std_velocity*dir_change', 'std_velocity*acceleration', 'std_acceleration*dir_change',
+        'iqr_velocity', 'iqr_acceleration', 'iqr_directional_change',
+        'iqr_velocity*dir_change', 'iqr_velocity*acceleration', 'iqr_acceleration*dir_change',
+        '90pcr_velocity', '90pcr_acceleration', '90pcr_directional_change',
+        '90pcr_velocity*dir_change', '90pcr_velocity*acceleration', '90pcr_acceleration*dir_change',
+        'median_velocity', 'median_acceleration', 'median_directional_change',
+        'median_velocity*dir_change', 'median_velocity*acceleration', 'median_acceleration*dir_change'
+    ]
+    return features
+
+
+def extract_trip_features(driver, trip):
+    trip_data = file_handling.load_trip_data(driver, trip)
+
+    transformed_data = transform_data(trip_data)
+
+    features = calculate_features(transformed_data, trip)
 
     return features
 
@@ -115,15 +282,15 @@ def transform_data(raw_data, plot=False):
     x_change = [0]
     y_change = [0]
     for i in range(1, temp_data.shape[0]):
-        x_change.append(temp_data[i, ix_x] - temp_data[i-1, ix_x])
-        y_change.append(temp_data[i, ix_y] - temp_data[i-1, ix_y])
+        x_change.append(temp_data[i, ix_x] - temp_data[i - 1, ix_x])
+        y_change.append(temp_data[i, ix_y] - temp_data[i - 1, ix_y])
 
     # Removing spikes and smoothing
     x_change_no_spikes = signal.medfilt(x_change)
     y_change_no_spikes = signal.medfilt(y_change)
 
     convolve_n = 3
-    convolve_array = np.ones((convolve_n, ))/convolve_n
+    convolve_array = np.ones((convolve_n, )) / convolve_n
     x_change_smooth = signal.convolve(x_change_no_spikes, convolve_array, mode='same')
     y_change_smooth = signal.convolve(y_change_no_spikes, convolve_array, mode='same')
 
@@ -152,26 +319,40 @@ def transform_data(raw_data, plot=False):
     # Acceleration
     acceleration = [0]
     for i in range(1, temp_data.shape[0]):
-        acceleration.append(temp_data[i, ix_velocity] - temp_data[i-1, ix_velocity])
+        acceleration.append(temp_data[i, ix_velocity] - temp_data[i - 1, ix_velocity])
     temp_data = np.column_stack((temp_data, acceleration))
     ix_acceleration = 5
 
     # Directional changes
-    directional_changes = calculation_direction_change(temp_data, ix_x_change, ix_y_change)
-    temp_data = np.column_stack((temp_data, directional_changes))
+    heading_changes = calculation_direction_change(temp_data, ix_x_change, ix_y_change)
+    temp_data = np.column_stack((temp_data, heading_changes))
     ix_direction_changes = 6
 
     # Interactions between speed, directional changes and acceleration
     speed_times_turn = []
     speed_times_acceleration = []
     acceleration_times_turn = []
+    speed_times_acceleration_times_turn = []
     for row in temp_data:
         speed_times_turn.append(row[ix_velocity] * row[ix_direction_changes])
         speed_times_acceleration.append(row[ix_velocity] * row[ix_acceleration])
         acceleration_times_turn.append(row[ix_acceleration] * row[ix_direction_changes])
-    temp_data = np.column_stack((temp_data, speed_times_turn, speed_times_acceleration, acceleration_times_turn))
+        speed_times_acceleration_times_turn.append(row[ix_velocity] * row[ix_acceleration] * row[ix_direction_changes])
 
-    transformed_data = np.copy(temp_data)
+    temp_data = np.column_stack((
+        temp_data,
+        speed_times_turn,
+        speed_times_acceleration,
+        acceleration_times_turn,
+        speed_times_acceleration_times_turn))
+    ix_speed_turns = 7
+    ix_speed_acc = 8
+    ix_acc_turns = 9
+    ix_speed_acc_turns = 10
+
+
+    # Index: speed = 4; acceleration = 5; turns = 6; speed*turns = 7; speed * acceleration = 8; acceleration * turns = 9
+    transformed_data = np.copy(temp_data[:, 4:])
 
     return transformed_data
 
@@ -196,7 +377,7 @@ def calculation_direction_change(data, index_1, index_2):
 
     for i in range(1, data.shape[0]):
         current_vector = [data[i, index_1], data[i, index_2]]
-        previous_vector = [data[i-1, index_1], data[i-1, index_2]]
+        previous_vector = [data[i - 1, index_1], data[i - 1, index_2]]
 
         radians = radians_between(current_vector, previous_vector)
 
@@ -232,7 +413,7 @@ def unit_vector(vector):
 
 if __name__ == '__main__':
     start_time = time.time()
-    test_data = build_data_set(1, rebuild_dataset=True)
+    test_data = build_driver_data(1, 5)
     print(test_data.shape)
     print("Elapsed = {0:.2f}".format(time.time() - start_time))
     print("done!")
