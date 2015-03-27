33d084f9bde0e1c5d25c4d9ef9496cca2aebd335
Niko Steinhoff <niko.steinhoff@gmail.com>
2015-03-27 16:39:34 +0100

    german annotations

--- src/plot_driver_sample.py
+++ src/plot_driver_sample.py
@@ -12,15 +12,15 @@ fake_trips = np.random.randn(10, 2) - 1
 
 fig, ax = plt.subplots()
 
-real, = ax.plot(driver_trips[:, 0], driver_trips[:, 1], 'og', label='real', alpha=0.75)
-fake, = ax.plot(fake_trips[:, 0], fake_trips[:, 1], 'or', label='fake', alpha=0.6)
+real, = ax.plot(driver_trips[:, 0], driver_trips[:, 1], 'o', label='Echt', alpha=1)
+fake, = ax.plot(fake_trips[:, 0], fake_trips[:, 1], 'o', label='Falsch', alpha=1)
 ax.legend()
 ax.tick_params(
     axis='both',
     reset=True,
     labelbottom='off',
     labelleft='off')
-ax.set_title('Real and fake trips')
+ax.set_title('Echte und Falsche Fahrten (vereinfachte Darstellung)')
 
 if len(sys.argv) > 1 and sys.argv[1] == 'noshow':
     print("Not showing plot! Saved to file.")
